cef9a432e4c334b2937cae0354950ac2645099c5
Thomas Kaegi <tk@meeloo.ch>
2016-05-30 09:26:16 +0200

    Fixes #8

---
+++ assets/plugins/cleaner.js
@@ -0,0 +1,25 @@
+$.Redactor.prototype.cleaner = function() {
+	return {
+		langs: {
+			de: {
+				"cleaner": "Formatierungen entfernen"
+			},
+			en: {
+				"cleaner": "Remove formats"
+			}
+		},
+		init: function() {
+			var button = this.button.add('cleaner', this.lang.get('cleaner'));
+			
+			this.button.addCallback(button, this.cleaner.set);
+		},
+		set: function() {
+			var selectedText = this.selection.text();
+			
+			if (selectedText != '') {
+				var selectedText = this.clean.stripTags(selectedText);
+				this.insert.html(selectedText);
+			}
+		}
+	};
+};
