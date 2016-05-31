3510e23eae8e5eafaa39d5c2bfc54eb063891da1
Thomas Kaegi <tk@meeloo.ch>
2016-05-31 09:21:05 +0200

    Fixes #2

---
+++ assets/plugins/internallink.js
@@ -0,0 +1,36 @@
+$.Redactor.prototype.internallink = function() {
+	return {
+		langs: {
+			de: {
+				"internallink": "Interner Link"
+			},
+			en: {
+				"internallink": "Internal link"
+			}
+		},
+		init: function() {
+			var button = this.button.add('internallink', this.lang.get('internallink'));
+			
+			this.button.addCallback(button, this.internallink.show);
+		},
+		show: function() {
+			var that = this;
+			var linkMap = openLinkMap();
+			$(linkMap).on('rex:selectLink', function (event, linkurl, linktext) {
+				event.preventDefault();
+				linkMap.close();
+				
+				that.internallink.set(linkurl, linktext);
+			});
+		},
+		set: function(linkurl, linktext) {
+			var selectedText = this.selection.text();
+			
+			if (selectedText != '') {
+				var linktext = selectedText;
+			}
+
+			this.insert.html('<a href="'+linkurl+'">'+linktext+'</a>');
+		}
+	};
+};
