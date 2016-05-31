7cf48edc44149e3353422fcb362e729d74095329
Thomas Kaegi <tk@meeloo.ch>
2016-05-31 16:23:40 +0200

    Fixes #4

---
+++ assets/plugins/medialink.js
@@ -0,0 +1,38 @@
+$.Redactor.prototype.medialink = function() {
+	return {
+		langs: {
+			de: {
+				"medialink": "Media Link"
+			},
+			en: {
+				"medialink": "Media link"
+			}
+		},
+		init: function() {
+			var button = this.button.add('medialink', this.lang.get('medialink'));
+			
+			this.button.addCallback(button, this.medialink.show);
+		},
+		show: function() {
+			var that = this;
+			var mediapool = openMediaPool('redactor_medialink');
+			$(mediapool).on('rex:selectMedia', function (event, filename) {
+				event.preventDefault();
+				mediapool.close();
+				
+				that.medialink.set(filename);
+			});
+		},
+		set: function(filename) {
+			var selectedText = this.selection.text();
+			
+			if (selectedText != '') {
+				var linktext = selectedText;
+			} else {
+				var linktext = filename;
+			}
+
+			this.insert.html('<a href="/media/'+filename+'">'+linktext+'</a>');
+		}
+	};
+};
