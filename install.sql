6c944484a14239a89f40d0f92b0301826e42d19f
Thomas Kaegi <tk@meeloo.ch>
2016-06-01 11:57:42 +0200

    minheight + maxheight hinzugefügt, Default-Button-Set angepasst

--- install.sql
+++ install.sql
@@ -5,12 +5,14 @@ CREATE TABLE `%TABLE_PREFIX%redactor2_profiles` (
   `name` varchar(30) NOT NULL DEFAULT '',
   `description` varchar(255) NOT NULL DEFAULT '',
   `urltype` varchar(50) NOT NULL,
+  `minheight` smallint(5) unsigned NOT NULL,
+  `maxheight` smallint(5) unsigned NOT NULL,
   `redactor_buttons` text NOT NULL,
   `redactor_plugins` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO `%TABLE_PREFIX%redactor2_profiles` (`id`, `name`, `description`, `urltype`, `redactor_plugins`) VALUES
-(1, 'full', 'Standard Redactor-Konfiguration', 'relative', 'cleaner,clips[Snippetname1=Snippettext1|Snippetname2=Snippettext2],fontcolor[Weiss=#ffffff|Schwarz=#000000],fontfamily[Arial|Times],fontsize[12px|15pt|120%],fullscreen,heading1,heading2,heading3,heading4,heading5,heading6,paragraph,rex_linkmap,rex_mediapool_image,rex_mediapool_link,table,textdirection,video');
+INSERT INTO `%TABLE_PREFIX%redactor2_profiles` (`id`, `name`, `description`, `minheight`, `maxheight`, `urltype`, `redactor_plugins`) VALUES
+(1, 'full', 'Standard Redactor-Konfiguration', 'relative', '300', '800', 'alignment,blockquote,bold,cleaner,clips[Snippetname1=Snippettext1|Snippetname2=Snippettext2],deleted,emaillink,externallink,fullscreen,heading1,heading2,heading3,heading4,heading5,heading6,horizontalrule,internallink,italic,limiter[300],media,medialink,orderedlist,properties,source,table,textdirection,underline,unorderedlist');
 
 ALTER TABLE `%TABLE_PREFIX%redactor2_profiles`
   ADD PRIMARY KEY (`id`);
