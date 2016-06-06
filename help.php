233ed15f1f5ac207310e62cb47288e5f0ad3d044
Thomas Kaegi <tk@meeloo.ch>
2016-06-06 08:42:47 +0200

    Hilfe-Text an Redactor 2 angepasst

--- help.php
+++ help.php
@@ -4,16 +4,27 @@
 	$code = '';
 	$code .= '<fieldset class="form-horizontal">'.PHP_EOL;
 	$code .= '  <div class="form-group">'.PHP_EOL;
-	$code .= '    <label class="col-sm-2 control-label" for="value1">VALUE 1</label>'.PHP_EOL;
+	$code .= '    <label class="col-sm-2 control-label" for="value-1">VALUE 1</label>'.PHP_EOL;
 	$code .= '    <div class="col-sm-10">'.PHP_EOL;
-	$code .= '      <textarea class="form-control redactorEditor2-full" id="value1" name="REX_INPUT_VALUE[1]">REX_VALUE[1]</textarea>'.PHP_EOL;
+	$code .= '      <textarea class="form-control redactorEditor2-full" id="value-1" name="REX_INPUT_VALUE[1]">REX_VALUE[1]</textarea>'.PHP_EOL;
 	$code .= '    </div>'.PHP_EOL;
 	$code .= '  </div>'.PHP_EOL;
-	$code .= '</fieldset>'.PHP_EOL;
+	$code .= '</fieldset>';
 	
 	$fragment = new rex_fragment();
 	$fragment->setVar('class', 'info', false);
 	$fragment->setVar('title', 'Beispiel: Module Input', false); //todo
+	$fragment->setVar('body', rex_string::highlight($code), false);
+	echo $fragment->parse('core/page/section.php');
+	
+	///
+	
+	$code = '';
+	$code .= 'REX_VALUE[id="1" output="html"]';
+	
+	$fragment = new rex_fragment();
+	$fragment->setVar('class', 'info', false);
+	$fragment->setVar('title', 'Beispiel: Module Output', false); //todo
 	$fragment->setVar('body', rex_string::highlight($code), false);
 	echo $fragment->parse('core/page/section.php');
 	
