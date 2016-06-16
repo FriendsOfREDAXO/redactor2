874d6515a1848699b856a440626489e750be6001
Thomas Kaegi <tk@meeloo.ch>
2016-06-16 10:58:21 +0200

    Formatierung normalisiert

--- assets/plugins/clips.js
+++ assets/plugins/clips.js
@@ -1,34 +1,31 @@
-(function($)
+$.Redactor.prototype.clips = function()
 {
-	$.Redactor.prototype.clips = function()
-	{
-		return {
-			init: function ()
-			{
-				if (!this.opts.clips) return;
-				var clips = this.opts.clips;
-				
-				var that = this;
-				var dropdown = {};
+	return {
+		init: function ()
+		{
+			if (!this.opts.clips) return;
+			var clips = this.opts.clips;
+			
+			var that = this;
+			var dropdown = {};
 
-				$.each(clips, function(i, s)
-				{
-					dropdown['s' + i] = {
-						title: s[0],
-						func: function() {
-							that.clips.set(s[1]);
-						}
-					};
-				});
-				
-				var button = this.button.add('clips', 'Clips');
-				this.button.setIcon(button, '<i class="fa fa-th-large"></i>');
-				this.button.addDropdown(button, dropdown);
-			},
-			set: function (value)
+			$.each(clips, function(i, s)
 			{
-				this.insert.html(value);
-			}
-		};
+				dropdown['s' + i] = {
+					title: s[0],
+					func: function() {
+						that.clips.set(s[1]);
+					}
+				};
+			});
+			
+			var button = this.button.add('clips', 'Clips');
+			this.button.setIcon(button, '<i class="fa fa-th-large"></i>');
+			this.button.addDropdown(button, dropdown);
+		},
+		set: function (value)
+		{
+			this.insert.html(value);
+		}
 	};
-})(jQuery);
+};
