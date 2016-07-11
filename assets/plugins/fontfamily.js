de828ff6692fc3b9f6ae8dd9700a7eaeadb3dfcb
Thomas Kaegi <tk@meeloo.ch>
2016-07-11 11:48:54 +0200

    Plugin 'Fontfamily' hinzugefügt
    Fixes #6

---
+++ assets/plugins/fontfamily.js
@@ -0,0 +1,48 @@
+$.Redactor.prototype.fontfamily = function() {
+	return {
+		langs: {
+			de: {
+				"fontfamily": "Schriftart",
+				"fontfamily_remove": "Schriftart entfernen",
+			},
+			en: {
+				"fontfamily": "Fontfamily",
+				"fontfamily_remove": "Remove fontfamily",
+			}
+		},
+		init: function () {
+			if (!this.opts.fontfamily) return;
+			var fonts = this.opts.fontfamily;
+			
+			var that = this;
+			var dropdown = {};
+			
+			$.each(fonts, function(i, s) {
+				dropdown['s' + i] = {
+					title: s,
+					func: function() {
+						that.fontfamily.set(s);
+					}
+				};
+			});
+			
+			dropdown['s' + fonts.length] = {
+				title: this.lang.get('fontfamily_remove'),
+				func: function() {
+					that.fontfamily.remove();
+				}
+			};
+			
+			var button = this.button.add('fontfamily', this.lang.get('fontfamily'));
+			this.button.setIcon(button, '<i class="fa fa-font"></i>');
+			this.button.addDropdown(button, dropdown);
+		},
+		set: function(value) {
+			this.inline.format('span', 'style', 'font-family:' + value + ';');
+		},
+		remove: function() {
+			this.inline.removeFormat();
+			//this.inline.removeStyleRule('font-family');
+		}
+	};
+};
