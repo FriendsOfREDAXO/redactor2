1561e01bc0b4e1f8d016e932ae7823a85b10ebe8
Thomas Kaegi <tk@meeloo.ch>
2016-07-11 11:50:13 +0200

    Buttons 'fontcolor', 'fontfamily' und 'fontsize' hinzugefügt

--- install.sql
+++ install.sql
@@ -12,7 +12,7 @@ CREATE TABLE `%TABLE_PREFIX%redactor2_profiles` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 INSERT INTO `%TABLE_PREFIX%redactor2_profiles` (`id`, `name`, `description`, `minheight`, `maxheight`, `characterlimit`, `urltype`, `redactor_plugins`) VALUES
-(1, 'full', 'Standard Redactor-Konfiguration', '300', '800', '0', 'relative', 'alignment,blockquote,bold,cleaner,clips[Snippetname1=Snippettext1|Snippetname2=Snippettext2],deleted,emaillink,externallink,fullscreen,groupheading[1|2|3|4|5|6],grouplink[email|external|internal|media],heading1,heading2,heading3,heading4,heading5,heading6,horizontalrule,internallink,italic,media,medialink,orderedlist,paragraph,properties,source,styles[code=Code|kbd=Shortcut|mark=Markiert|samp=Sample|var=Variable],sub,sup,table,textdirection,underline,unorderedlist');
+(1, 'full', 'Standard Redactor-Konfiguration', '300', '800', '0', 'relative', 'alignment,blockquote,bold,cleaner,clips[Snippetname1=Snippettext1|Snippetname2=Snippettext2],deleted,emaillink,externallink,fontcolor[Weiss=#ffffff|Schwarz=#000000],fontfamily[Arial|Times],fontsize[12px|15pt|120%],fullscreen,groupheading[1|2|3|4|5|6],grouplink[email|external|internal|media],heading1,heading2,heading3,heading4,heading5,heading6,horizontalrule,internallink,italic,media,medialink,orderedlist,paragraph,properties,source,styles[code=Code|kbd=Shortcut|mark=Markiert|samp=Sample|var=Variable],sub,sup,table,textdirection,underline,unorderedlist');
 
 ALTER TABLE `%TABLE_PREFIX%redactor2_profiles`
   ADD PRIMARY KEY (`id`);
