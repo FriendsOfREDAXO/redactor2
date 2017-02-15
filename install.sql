dadf30beb51f57947a8b2b05c73e715198fec782
Thomas Kaegi <tk@walderwerber.ch>
2017-02-15 08:32:44 +0100

    Felder 'toolbarfixed' und 'shortcuts' hinzugefügt

--- install.sql
+++ install.sql
@@ -8,11 +8,13 @@ CREATE TABLE `%TABLE_PREFIX%redactor2_profiles` (
   `minheight` smallint(5) unsigned NOT NULL,
   `maxheight` smallint(5) unsigned NOT NULL,
   `characterlimit` smallint(5) unsigned NOT NULL,
+  `toolbarfixed` tinyint(1) unsigned NOT NULL,
+  `shortcuts` tinyint(1) unsigned NOT NULL,
   `redactor_plugins` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 INSERT INTO `%TABLE_PREFIX%redactor2_profiles` (`id`, `name`, `description`, `minheight`, `maxheight`, `characterlimit`, `urltype`, `redactor_plugins`) VALUES
-(1, 'full', 'Standard Redactor-Konfiguration', '300', '800', '0', 'relative', 'alignment,blockquote,bold,cleaner,clips[Snippetname1=Snippettext1|Snippetname2=Snippettext2],deleted,emaillink,externallink,fontcolor[Weiss=#ffffff|Schwarz=#000000],fontfamily[Arial|Times],fontsize[12px|15pt|120%],fullscreen,groupheading[1|2|3|4|5|6],grouplink[email|external|internal|media],heading1,heading2,heading3,heading4,heading5,heading6,horizontalrule,internallink,italic,media,medialink,orderedlist,paragraph,properties,redo,source,styles[code=Code|kbd=Shortcut|mark=Markiert|samp=Sample|var=Variable],sub,sup,table,textdirection,underline,undo,unorderedlist');
+(1, 'full', 'Standard Redactor-Konfiguration', '300', '800', '0', 'relative', 'alignment,blockquote,bold,cleaner,clips[Snippetname1=Snippettext1|Snippetname2=Snippettext2],deleted,emaillink,externallink,fontcolor[Weiss=#ffffff|Schwarz=#000000],fontfamily[Arial|Times],fontsize[12px|15pt|120%],fullscreen,groupheading[1|2|3|4|5|6],grouplink[email|external|internal|media],grouplist[unorderedlist|orderedlist|indent|outdent],heading1,heading2,heading3,heading4,heading5,heading6,horizontalrule,internallink,italic,media,medialink,orderedlist,paragraph,properties,redo,source,styles[code=Code|kbd=Shortcut|mark=Markiert|samp=Sample|var=Variable],sub,sup,table,textdirection,underline,undo,unorderedlist');
 
 ALTER TABLE `%TABLE_PREFIX%redactor2_profiles`
   ADD PRIMARY KEY (`id`);
---
+++ update.php
@@ -0,0 +1,6 @@
+<?php
+	rex_sql_table::get(rex::getTable('redactor2_profiles'))
+	->ensureColumn(new rex_sql_column('toolbarfixed', 'tinyint(1)'))
+	->ensureColumn(new rex_sql_column('shortcuts', 'tinyint(1)'))
+	->alter();
+?>
