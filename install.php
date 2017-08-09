b9480029e5bb7da3a39390d8198bd31086abd193
Alex Platter <a.platter@kreatif.it>
2017-08-09 16:06:30 +0200

    dependencies upate

--- install.php
+++ install.php
@@ -2,4 +2,5 @@
 
 \rex_sql_table::get(\rex::getTable('redactor2_profiles'))
     ->ensureColumn(new \rex_sql_column('linkify', 'tinyint', false, null, 'unsigned'))
+    ->ensureColumn(new \rex_sql_column('redactor_customplugins', 'text', false, ''))
     ->alter();
