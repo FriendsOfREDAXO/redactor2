e6b83ad0fb4ca2166e2e970b1e34e6bac78ecaf8
Alex Platter <a.platter@kreatif.it>
2017-10-13 15:35:27 +0200

    default value fixed

--- install.php
+++ install.php
@@ -1,6 +1,6 @@
 <?php
 
 \rex_sql_table::get(\rex::getTable('redactor2_profiles'))
-    ->ensureColumn(new \rex_sql_column('linkify', 'tinyint', false, 0, 'unsigned'))
+    ->ensureColumn(new \rex_sql_column('linkify', 'tinyint', true, 0, 'unsigned'))
     ->ensureColumn(new \rex_sql_column('redactor_customplugins', 'text', false, ''))
     ->alter();
