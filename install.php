7a806ef49ce17b4be2881ee14aebb58c30169383
Thomas Kaegi <tk@walderwerber.ch>
2017-12-12 07:41:16 +0100

    Datei entfernt, da es die update.php gibt
    Fixes #109

--- install.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-\rex_sql_table::get(\rex::getTable('redactor2_profiles'))
-    ->ensureColumn(new \rex_sql_column('linkify', 'tinyint', true, 0, 'unsigned'))
-    ->ensureColumn(new \rex_sql_column('redactor_customplugins', 'text', false, ''))
-    ->alter();
