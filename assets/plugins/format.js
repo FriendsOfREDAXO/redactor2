8d40ef56446cc798d4a750b60705a9b6f423e190
Peter Thiel <thiel.peter@googlemail.com>
2018-01-18 15:54:44 +0100

    Create format.js

---
+++ assets/plugins/format.js
@@ -0,0 +1,37 @@
+$.Redactor.prototype.format = function() {
+	return {
+		langs: {
+			de: {
+				"format": "Format"
+			},
+			en: {
+				"format": "Format"
+			}
+		},
+		init: function() {
+			
+			if (!this.opts.format) return;
+			var format = this.opts.format;
+			
+			var that = this;
+			var dropdown = {};
+			
+			$.each(format, function(i, s)
+			{
+				dropdown[i] = {
+					title: s[0],
+					func: function() {
+						that.format.set(s[1]);
+					}
+				};
+			});
+			
+			var button = this.button.add('format', this.lang.get('format'));
+			this.button.setIcon(button, '<i class="re-icon-format"></i>');
+			this.button.addDropdown(button, dropdown);
+		},
+		set: function(args) {
+			this.block.format(args.split(".")[0], 'class', args.split(".")[1]);
+		}
+	};
+};
