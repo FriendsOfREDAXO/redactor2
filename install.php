b182a25290c8d414ac2197b2bb39e1b37dc7ac4e
Markus Staab <maggus.staab@googlemail.com>
2018-12-15 15:56:38 +0100

    Update install.php
    Co-Authored-By: dergel <jan.kristinus@yakamara.de>

--- install.php
+++ install.php
@@ -20,5 +20,3 @@ rex_sql::factory()->setQuery('INSERT IGNORE INTO '.rex::getTable('redactor2_prof
 (1, "full", "Standard Redactor-Konfiguration", "300", "800", "0", "relative", "blank", "0", "1", "", "anchorlink,alignment,blockquote,bold,cleaner,clips[Snippetname1=Snippettext1|Snippetname2=Snippettext2],deleted,emaillink,externallink,fontcolor[Weiss=#ffffff|Schwarz=#000000],fontfamily[Arial|Times],fontsize[12px|15pt|120%],format[Absatz Klein=p.small|Absatz Mittel=p.middle|Absatz Gross=p.big],fullscreen,groupheading[1|2|3|4|5|6],grouplink[email|external|internal|media|telephone],grouplist[unorderedlist|orderedlist|indent|outdent],heading1,heading2,heading3,heading4,heading5,heading6,horizontalrule,internallink,italic,media,medialink,orderedlist,paragraph,properties,redo,source,styles[code=Code|kbd=Shortcut|mark=Markiert|samp=Sample|var=Variable],sub,sup,table,telephonelink,textdirection,underline,undo,unorderedlist");');
 
 redactor2::createJavascriptFile();
-
-?>
