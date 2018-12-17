59c8d915083c66376eeb6a97eb166a0b2fce8ff5
Thomas Skerbis <thomas.skerbis@mac.com>
2018-12-17 22:12:48 +0100

    readme einbinden

---
+++ pages/help.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * This file is part of the redactor2 package.
+ *
+ * @author (c) Friends Of REDAXO
+ * @author <friendsof@redaxo.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+$file = rex_file::get(rex_path::addon('phpmailer', 'README.md'));
+$body = rex_markdown::factory()->parse($file);
+$fragment = new rex_fragment();
+$fragment->setVar('body', $body, false);
+$help = $fragment->parse('core/page/section.php');
+echo $help;
+?>
